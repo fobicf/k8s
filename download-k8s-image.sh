c7a5711b64b37305cf43c289e0b313b1b6f3c417
fobicf <fobicf@126.com>
2017-09-19 18:11:38 +0800

    1.0.1

---
+++ download-k8s-image.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+images=(
+    kube-proxy-amd64:v1.7.6
+    kube-controller-manager-amd64:v1.7.6
+    kube-apiserver-amd64:v1.7.6
+    kube-scheduler-amd64:v1.7.6
+    kubernetes-dashboard-amd64:v1.6.3
+    k8s-dns-sidecar-amd64:1.14.1
+    k8s-dns-kube-dns-amd64:1.14.1
+    k8s-dns-dnsmasq-nanny-amd64:1.14.1
+    etcd-amd64:3.0.17
+    pause-amd64:3.0
+)
+
+for imageName in ${images[@]} ; do
+    docker pull gcr.io/google_containers/$imageName
+    docker tag gcr.io/google_containers/$imageName registry.cn-beijing.aliyuncs.com/bbt_k8s/$imageName
+    docker push registry.cn-beijing.aliyuncs.com/bbt_k8s/$imageName
+done
