25f57c9ba023cf6d876595712025c86d33cc586b
fobicf <fobicf@126.com>
2017-09-26 15:14:06 +0800

    1.7.3

--- download-k8s-image.sh
+++ download-k8s-image.sh
@@ -5,15 +5,17 @@ images=(
     kube-controller-manager-amd64:v1.7.6
     kube-apiserver-amd64:v1.7.6
     kube-scheduler-amd64:v1.7.6
-    kubernetes-dashboard-amd64:v1.6.3
-    k8s-dns-sidecar-amd64:1.14.1
-    k8s-dns-kube-dns-amd64:1.14.1
-    k8s-dns-dnsmasq-nanny-amd64:1.14.1
+    kubernetes-dashboard-amd64:v1.7.0
+    k8s-dns-sidecar-amd64:1.14.4
+    k8s-dns-kube-dns-amd64:1.14.4
+    k8s-dns-dnsmasq-nanny-amd64:1.14.4
     etcd-amd64:3.0.17
     pause-amd64:3.0
+	kubernetes-dashboard-init-amd64:v1.0.0
 )
 
 for imageName in ${images[@]} ; do
     docker pull docker.io/fobicf/$imageName
     docker tag docker.io/fobicf/$imageName gcr.io/google_containers/$imageName
+    docker rmi docker.io/fobicf/$imageName
 done
